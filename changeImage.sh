da934ac8ca2c3c0907d21ee0f21194209592a19b
Lucy <lrbrantle@gmail.com>
2019-02-01 22:26:09 -0800

    cleanup

--- changeImage.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-for file in ./*.png; do
-	#echo $file
-	python3 imageChange.py $file
-	rm $file
-done
--- imageChange.py
+++
@@ -1,8 +0,0 @@
-import imageio
-import sys
-from os import path
-import numpy as np
-
-name, ext = path.splitext(sys.argv[1])
-img = imageio.imread(sys.argv[1])
-imageio.imwrite(name + "code" + ext, np.hstack((img[:,1:29], img[:,36:64])))
