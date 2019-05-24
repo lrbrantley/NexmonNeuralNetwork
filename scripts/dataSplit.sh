c61682d4b22218160e4e4ead21075d38f1df4376
Thomas Flucke <tomflucke@gmail.com>
2019-05-24 16:53:37 -0700

    Refined script UI and added a script to fully process pcaps.

--- scripts/dataSplit.sh
+++ scripts/dataSplit.sh
@@ -1,11 +1,30 @@
 #! /bin/bash
 
-for file in ./*.png; do
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <data dir> <label> [<pngs> ...]" 1>&2
+    echo "If no pngs are provided, then they are assumed to be in the current" 1>&2
+    echo "directory." 1>&2
+    exit 1
+fi
+
+readonly DATA_DIR="$1"
+readonly LABEL="$2"
+shift 2
+images="$@"
+if [ $# -eq 0 ]; then
+    images="./*.png"
+fi
+
+readonly TRAIN_DIR="$DATA_DIR/train/$LABEL"
+readonly TEST_DIR="$DATA_DIR/validation/$LABEL"
+mkdir -p "$TRAIN_DIR" "$TEST_DIR"
+
+for file in $images; do
 	val=$RANDOM
 	let "val %= 100"
 	if test $val -le 70; then
-		mv "$file" ./data/train
+		mv "$file" "$TRAIN_DIR"
 	else
-		mv "$file" ./data/validation
+		mv "$file" "$TEST_DIR"
 	fi
 done
--- scripts/preprocessCSV.py
+++ scripts/preprocessCSV.py
@@ -6,23 +6,42 @@ from os import path
 import numpy as np
 import os
 import imageio
+import argparse
 
-def scaleRow(row):
+def parse_args():
+        parser = argparse.ArgumentParser(
+                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        parser.add_argument('csv', type=argparse.FileType('r'), help='CSV file')
+        parser.add_argument('outformat', nargs='?', help='Output filename format')
+        parser.add_argument('--version', action='version', version='%(prog)s 1.1')
+        args = parser.parse_args()
+        if args.outformat is None:
+                name, ext = path.splitext(args.csv)
+                args.outformat = "%s-%%d.png" % name
+        return args
+
+def scale_row(row):
+        # TODO: Parameterize columns taken
         trimmed = row[3:31]+row[38:65]
+        # TODO: Error if column count wrong
         #print(trimmed)
-        floats = [float(s.replace('(', '').replace('+0j)', '').replace('0j', '0')) for s in trimmed]
+        floats = [float(s.replace('(', '').replace('+0j)', '').replace('0j', '0'))
+                  for s in trimmed]
         minV = -min(floats)
         scale = 255/(max(floats) + minV)
         return [int((i + minV)*scale) for i in floats]
 
-name, ext = path.splitext(sys.argv[1])
-i = 1
-with open(sys.argv[1], 'r') as inFile:
-        #3-31, 38-65
+def preprocess_file(inFile, outFormat):
+        i = 1
         out = []
         for row in csv.reader(inFile):
-                out.append(scaleRow(row))
+                out.append(scale_row(row))
                 if len(out) >= 400:
-                        imageio.imwrite("%s-%d.png" % (name, i), np.array(out, dtype=np.uint8))
+                        imageio.imwrite(outFormat % i,
+                                        np.array(out, dtype=np.uint8))
                         i += 1
                         out = []
+
+if __name__ == "__main__":
+        args = parse_args()
+        preprocess_file(args.csv, args.outformat)
